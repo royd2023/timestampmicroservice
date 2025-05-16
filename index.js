02b7e17c2ab04ba8d51f7f3ef3c0c22597a9a726
royd2023 <rdinh2023@gmail.com>
2025-05-16 16:04:39 -0400

    Finished project in index.js

--- index.js
+++ index.js
@@ -24,6 +24,42 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// API endpoint to return the current timestamp
+// resquest to /api/ will return the current date and time in UTC format
+// and the unix timestamp 
+app.get("/api/:date?", function (req, res) {
+  // req.params.date is the date parameter passed in the URL
+  const dateParam = req.params.date;
+  // If no date parameter is provided, return the current date and time 
+  // in UTC format and the unix timestamp
+  if (!dateParam) {
+    const now = new Date();
+    res.json({
+      unix: now.getTime(),
+      utc: now.toUTCString()
+    });
+  }
+  // If the date parameter is provided, check if it is a valid date 
+  else {
+    let date;
+    // /^\d+$/ is a regular expression that matches a string that contains only digits
+    if (/^\d+$/.test(dateParam)) {
+      // convert the date parameter to a number
+      date = new Date(parseInt(dateParam));
+    } else {
+      date = new Date(dateParam);
+    }
+    // If the date is invalid, return an error message
+    if (isNaN(date.getTime())) {
+      res.json({error: "Invalid Date"});
+    } else {
+      res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+      }); 
+    }
+  }
+})
 
 
 // Listen on port set in environment variable or default to 3000
